9f78271c5231751161c3e68f0fdd7fe4dfc55a2f
jgwill <jgi@jgwill.com>
2018-10-23 23:35:26 -0400

    added aliases

--- bashrc-alias.sh
+++ bashrc-alias.sh
@@ -5,9 +5,15 @@
 
 alias d=docker
 alias dcl='docker container ps -a'
+alias dps=dcl
 alias dcstats='docker stats'
-
-
+alias dcs='docker stats'
+alias dr='docker run'
+alias drm='docker rm '
+alias drmi='docker rmi '
+alias dlog='docker logs '
+alias di='docker images'
+alias dimg=di
 #path
 
 
